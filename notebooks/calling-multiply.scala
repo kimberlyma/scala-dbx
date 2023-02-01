11399d5e5f81c6d9194f34d58e2c8b030e0bae8d
Kimberly Mahoney <mahoneykimberlya@gmail.com>
2023-01-31 17:12:03 -0800

    adding scala notebook

--- notebooks/calling-multiply.scala
+++ notebooks/calling-multiply.scala
@@ -1,1 +1,12 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC
+// MAGIC # Sample notebook
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC CREATE TABLE uc_demos_kimberly_mahoney.default.parq2 (key STRING, group STRING, value INT)
+// MAGIC USING PARQUET
+// MAGIC LOCATION 's3://databricks-kmahoney-uc/parq2/';
 
